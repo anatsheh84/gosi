7e39f3e1044c4685feac5773855109cc43f15e20
anatsheh84 <nat.ahmad@gmail.com>
2024-09-15 16:40:55 +0300

    zz

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,4 +1,4 @@
 <?php
   print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
-  print "\n This is GOSI Demo" . "\n";
+  ##print "\n This is GOSI Demo" . "\n";
 ?>
